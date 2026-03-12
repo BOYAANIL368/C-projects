472db3fe628745a1ffc7c77c7edafb35b37ecd54
BOYAANIL368 <boyaanilkumar2003@gmail.com>
2026-03-12 12:03:12 +0530

    Add files via upload

--- stud management system using inked list/include/header.h
+++ stud management system using inked list/include/header.h
@@ -1,1 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct st
+{
+        int roll;
+        char name[20];
+        float perce;
+        struct st *next;
+};
+void stud_add(struct st **);
+void stud_show(struct st *);
+void delate_all(struct st **);
+void stud_del(struct st **);
+void stud_save(struct st *,char *);
+void read_file(struct st **,char *);
+void rev_list(struct st **);
+void stud_sort(struct st *);
+void stud_modif(struct st **);
+
 
